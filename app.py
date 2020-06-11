32b5d6562d00243c4bdfd1b783bc423a0f898d3d
Colm Doyle <collydoyle_88@hotmail.com>
2020-06-11 19:11:48 +0000

    Initial Commit

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+import os
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Milestone 3 Project"
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=(os.environ.get('PORT')),
+            debug=True)
