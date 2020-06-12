6f0e0c72bd9719edd8ac02284633742ce2bc53b5
Colm Doyle <collydoyle_88@hotmail.com>
2020-06-12 20:50:52 +0000

    initial commit

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+# app.config["MONGO_DBNAME"] = 'milestone-3'
+# app.config["MONGO_URI"] = 'mongodb+srv://root:rootUser@myfirstcluster-a08kd.mongodb.net/milestone-3?retryWrites=true&w=majority'
+app.config["MONGO_DBNAME"] = 'milestone-3'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI', "Env Not loaded")
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return "Milestone 3 Project"
+@app.route('/get_users')
+def get_users():
+    return render_template("users.html", users=mongo.db.users.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=(os.environ.get('PORT')),
-            debug=True)
+            debug=False)
